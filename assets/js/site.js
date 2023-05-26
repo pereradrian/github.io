c4a4ed75fe1f1d65923731884d3887e5b13cbc70
Bill Basiles <pereradrian@gmail.com>
2023-05-26 21:51:17 +0200

    Added a simple password generator

--- assets/js/site.js
+++ assets/js/site.js
@@ -10,3 +10,25 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+var password = document.getElementById("password-generated");
+
+function genPassword() {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+    var passwordLength = document.getElementById("password-length").value;
+    var password = "";
+    if (passwordLength > 200) {
+        passwordLength = 200;
+    }
+    for (var i = 0; i < passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById("password-generated").value = password;
+}
+
+function copyPassword() {
+    var copyText = document.getElementById("password-generated");
+    copyText.select();
+    document.execCommand("copy");
+}

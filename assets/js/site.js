e0172c9f0fafebfcd15814fdc9ca955f5f2dbf88
Bill Basiles <pereradrian@gmail.com>
2023-05-26 21:27:36 +0200

    Several improvements

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,12 @@
 $(".dropbtn").on("mouseover", function() {
     $(".dropdown-content").toggleClass("hidden");
 });
+
+/* side navigation functionality */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}

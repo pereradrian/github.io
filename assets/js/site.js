c9489c8c4135b33cfce116358f2a5f5ed659ed32
adrian <adrian.munnoz@qrr.es>
2024-11-07 10:24:05 +0100

    Implemented visit count (to be tested)  - Several minor fixes, some still to be solved

--- assets/js/site.js
+++ assets/js/site.js
@@ -57,32 +57,32 @@ function testRegexp() {
 
 /* Image upload */
 const fileinput = document.getElementById('fileinput')
-
 const canvas = document.getElementById('canvas')
-
-const ctx = canvas.getContext('2d')
-
-const srcImage = new Image
-
-let imgData = null
-
-let originalPixels = null
-
-fileinput.onchange = function(e) {
-    if (e.target.files && e.target.files.item(0)) {
-        srcImage.src = URL.createObjectURL(e.target.files[0])
+if (canvas) {
+    const ctx = canvas.getContext('2d')
+    
+    const srcImage = new Image
+    
+    let imgData = null
+    
+    let originalPixels = null
+    
+    fileinput.onchange = function(e) {
+        if (e.target.files && e.target.files.item(0)) {
+            srcImage.src = URL.createObjectURL(e.target.files[0])
+        }
     }
-}
-
-srcImage.onload = function() {
-    canvas.width = srcImage.width
-    canvas.height = srcImage.height
-    ctx.drawImage(srcImage, 0, 0, srcImage.width, srcImage.height)
-    imgData = ctx.getImageData(0, 0, srcImage.width, srcImage.height)
-    originalPixels = imgData.data.slice()
-
-    /* Process to text */
-    var n_columns = parseFloat(document.getElementById("ascii-art-columns").value);
-    /* TODO */
-
+    
+    srcImage.onload = function() {
+        canvas.width = srcImage.width
+        canvas.height = srcImage.height
+        ctx.drawImage(srcImage, 0, 0, srcImage.width, srcImage.height)
+        imgData = ctx.getImageData(0, 0, srcImage.width, srcImage.height)
+        originalPixels = imgData.data.slice()
+    
+        /* Process to text */
+        var n_columns = parseFloat(document.getElementById("ascii-art-columns").value);
+        /* TODO */
+    
+    }
 }

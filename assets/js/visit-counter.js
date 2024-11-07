c8c17b6b02e6b570bc323710258d4ce142ef2828
adrian <adrian.munnoz@qrr.es>
2024-11-07 11:53:03 +0100

    Fix: gh actions cannot be used, swapped to google analytics

--- assets/js/visit-counter.js
+++
@@ -1,6 +0,0 @@
-fetch("https://raw.githubusercontent.com/pereradrian/pereradrian.github.io/main/assets/data/visit-counter.json")
-.then(response => response.json())
-.then(data => {
-  document.getElementById("visit-counter").innerText = data.visits;
-})
-.catch(error => console.error("Error loading counter:", error));
